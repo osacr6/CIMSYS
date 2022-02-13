c20a5075c5b03805c6c0911ec5608e7168a7095b
Daniel Murillo <d.murillo.porras@accenture.com>
2022-02-12 21:58:15 -0600

    Tarea Semana 3

---
+++ src/test/java/com/CIMSYS/CimsysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CIMSYS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CimsysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
